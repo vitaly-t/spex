1c1c3a98277ffbc5b25ae036155c99c87eb8095d
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2015-09-14 01:41:24 +0100

    initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Specialized Promise Extensions
+
+This project is a work in progress.
+
+Please visit us later ;)
---
+++ gruntfile.js
@@ -0,0 +1,16 @@
+"use strict";
+module.exports = function (grunt) {
+    grunt.initConfig({
+        jsdoc2md: {
+            oneOutputFile: {
+                options: {
+                    "no-gfm": true
+                },
+                src: "lib/**/*.js",
+                dest: "API.md"
+            }
+        }
+    });
+    grunt.loadNpmTasks("grunt-jsdoc-to-markdown");
+    grunt.registerTask("default", "jsdoc2md");
+};
