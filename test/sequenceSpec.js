128227f0648c31dcdf5b370cbc03e4663034449b
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2015-09-21 00:27:20 +0100

    started adding tests.

--- test/sequenceSpec.js
+++ test/sequenceSpec.js
@@ -4,6 +4,34 @@ var lib = require('./header');
 var promise = lib.promise;
 var spex = lib.main(promise);
 
+var dummy = function () {
+};
+
 describe("Sequence", function () {
+
+    describe("source error", function () {
+
+        var r, msg = "source error";
+        beforeEach(function (done) {
+            function source() {
+                throw msg;
+            }
+
+            spex.sequence(source)
+                .catch(function (reason) {
+                    r = reason;
+                    done();
+                })
+
+        });
+
+        it("must reject correctly", function () {
+            expect(r).toEqual({
+                index: 0,
+                error: msg,
+                source: undefined
+            });
+        })
+    });
 
 });
