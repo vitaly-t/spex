cbbbceae31257c7bab0617ff3bfc3a60162efc19
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2015-09-20 17:14:14 +0100

    docs update.

--- gruntfile.js
+++ gruntfile.js
@@ -2,6 +2,8 @@
 
 var fs = require("fs");
 
+var mdFile = "API.md";
+
 module.exports = function (grunt) {
     grunt.initConfig({
         jsdoc2md: {
@@ -10,18 +12,31 @@ module.exports = function (grunt) {
                     "no-gfm": true
                 },
                 src: "lib/**/*.js",
-                dest: "API.md"
+                dest: mdFile
             }
         }
     });
 
-    grunt.registerTask("appendLinks", function () {
+    grunt.registerTask("fixLinks", function () {
         var done = this.async();
-        fs.readFile("static/links.md", "utf-8", function (err, data) {
-            fs.appendFile("API.md", data, done);
+        fs.readFile(mdFile, "utf-8", function (err, data) {
+            data = data.replace(/\$\[[a-z0-9\s]+\]/gi, function (name) {
+                var svn = name.replace(/\$\[|\]/g, ''); // stripped variable name;
+                if (svn in links) {
+                    return "{@link " + links[svn] + "|" + svn + "}";
+                } else {
+                    return name;
+                }
+            });
+            fs.writeFile(mdFile, data, done);
         });
     });
 
     grunt.loadNpmTasks("grunt-jsdoc-to-markdown");
-    grunt.registerTask("default", ["jsdoc2md", "appendLinks"]);
+    grunt.registerTask("default", ["jsdoc2md", "fixLinks"]);
+};
+
+var links = {
+    "mixed value": "https://github.com/vitaly-t/spex/wiki/Mixed-Values",
+    "mixed values": "https://github.com/vitaly-t/spex/wiki/Mixed-Values"
 };
